345c69657616e4a3a3ae415d847b84219b0f6558
Romberto <korhoff@mail.ru>
2025-03-02 00:29:55 +0400

    added: photo to the main home

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -2,5 +2,9 @@ import React from "react";
 import styled from "./Home.module.scss";
 
 export const Home: React.FC = () => {
-  return <div className={styled.home}></div>;
+  return (
+    <div className={styled.home}>
+      <img src="https://static.tildacdn.com/tild3064-3933-4665-b933-633336306563/photo.jpg" />
+    </div>
+  );
 };

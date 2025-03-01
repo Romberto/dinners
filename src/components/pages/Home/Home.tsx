f6524e25614185e02fb32e57b62ae8f8af2f3cb8
Romberto <korhoff@mail.ru>
2025-03-02 00:46:02 +0400

    added: add swiper

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -4,6 +4,9 @@ import styled from "./Home.module.scss";
 export const Home: React.FC = () => {
   return (
     <div className={styled.home}>
+      <div>
+        
+      </div>
       <img src="https://static.tildacdn.com/tild3064-3933-4665-b933-633336306563/photo.jpg" />
     </div>
   );
